c8d9917c289a635211e8759902c1c4f5047014eb
Danilo Tosic <54001767+ToskePG@users.noreply.github.com>
2025-01-15 14:48:05 +0100

    Deletes wrong files.

--- journal/success.php
+++
@@ -1,9 +0,0 @@
-﻿<?
-/*session_start();
-if(!session_is_registered(myusername)){
-header("location:main_login.php");
-}*/
-?>
-
-<p>Thank you for your subscription to our newsletter.</p>
-
--- journal/under-construction.php
+++
@@ -1,4 +0,0 @@
-﻿<div style="text-align:center">
-<img src="images/under-construction.jpg" /><br />
-<p align="center">Under construction</p>
-</div>
